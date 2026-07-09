c38b63d20b8de11568370c13b5cde2c4c09b1826
Gethray99 <98901059+Gethray99@users.noreply.github.com>
2026-07-09 12:35:23 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import os 
+from fastmcp import FastMCP
+import chromadb
+from llama_cloud_services import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PERSISTENT_CLIENT = "./chroma_db"
+COLLECTION_NAME = "rag_mcp"
+DATA_DIR = "./data"
+LLAMA_API = os.getenv("LLAMA_CLOUD_API_KEY", "")
+
+mcp = FastMCP("RAG Server")
+
+def init_chroma():
+  client = chromadb.PersistentClient(path=PERSISTENT_CLIENT)
+
+  collection = client.get_or_create_collection(name=COLLECTION_NAME)
+
+  return client, collection
+
+
+@mcp.tool(timeout=600)
+def ingest_data():
+  """
+  Initialize chromadb and collections so that the user can query them later.
+  """
+
+  client, collection = init_chroma()
+  client.delete_collection(name=COLLECTION_NAME)
+  new_collection = client.get_or_create_collection(name=COLLECTION_NAME)
+
+
+  parser = LlamaParse(api_key=LLAMA_API, result_type="text")
+
+  file_extractor = {'.pdf' : parser}
+
+  documents = SimpleDirectoryReader(DATA_DIR, file_extractor=file_extractor).load_data()
+
+  for doc in documents:
+    new_collection.add(
+      documents=[doc.text],
+      metadatas=[doc.metadata],
+      ids=[doc.doc_id]
+    )
+
+  final_count = new_collection.count()
+  return f"Final count: {final_count}"
+
+
+@mcp.tool(timeout=600)
+def query_data(query:str , n_result:int):
+   """
+   Query the vector database for documents for documents similar to the query.
+   """
+
+
+   client , collection = init_chroma()
+
+   collection = client.get_collection(name=COLLECTION_NAME)
+
+   results = collection.query(
+      query_texts=[query],
+      n_results= 2,
+      include=["documents", "metadatas", "distances"]
+   )
+
+   if results == None or len(results) == 0:
+      return "No results found"
+
+   return results
+
+
+@mcp.tool(timeout=600)
+def get_db_status():
+  """
+  Get the status of the vector database.
+  """
+
+  chroma_client, chroma_collection = init_chroma()
+  count = chroma_collection.count()
+  return f"Database status: {count} documents ingested."
+
+if __name__ == "__main__":
+  init_chroma()
+  mcp.run("streamable-http")
